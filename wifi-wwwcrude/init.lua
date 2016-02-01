6a7ab9ffe41b629bd8ec32a5ed70c94640160b5c
Travis Hardiman <travis.hardiman@gmail.com>
2016-01-31 23:24:23 -0500

    very crude web server works!

---
+++ wifi-wwwcrude/init.lua
@@ -0,0 +1,44 @@
+print("\n")
+print("ESP8266 Started.")
+
+-- print out some debugging info:
+majorVer, minorVer, devVer, chipid, flashid, flashsize, flashmode, flashspeed = node.info();
+print("NodeMCU "..majorVer.."."..minorVer.."."..devVer)
+print("Chip "..chipid)
+print("Flash "..flashid.." size:"..flashsize.." mode:"..flashmode.." speed:"..flashspeed)
+
+print("Configuring wifi...")
+-- set the ssid and password to the values for the network that you are connecting to
+cfg = {
+    ssid = "WifiName",
+    password = "WifiPassword"
+}
+
+wifi.setmode(wifi.STATION)
+wifi.sta.config(cfg.ssid, cfg.password)
+ip = nil
+wifi.sta.connect()
+
+-- set timer to wait for an IP address
+tmr.alarm(0, 1000, 1, function()
+    print("wifi status: "..wifi.sta.status())
+    ip = wifi.sta.getip() 
+    if ip == nil then
+        print("connecting...") 
+    else
+        -- IP isn't nil, stop timer
+        tmr.stop(0)
+
+        print('ip: ', ip)
+
+        -- compile and execute www code
+        print("Compiling...")
+        node.compile("www.lua")
+
+        print("Running...")
+        dofile("www.lc")
+
+        print("Open this URL in a browser: ")
+        print("http://"..ip.."/")
+    end
+end)
---
+++ wifi-wwwcrude/www.lua
@@ -0,0 +1,38 @@
+-- www.lua
+www = net.createServer(net.TCP)
+
+www:listen(80, function(sock)
+  sock:on("receive", function(client, request)
+    print(request)
+    
+    sock:send("HTTP/1.1 200 OK\n"..
+"Server: ESP8266 Webserver\n"..
+"Connection: close\n"..
+"Content-Type: text/html\n"..
+"\n")
+
+    sock:send("<!doctype html>\n"..
+"<html lang='en'>"..
+"    <head>"..
+"        <meta charset='utf-8' />"..
+"        <title>ESP8266</title>"..
+"        <meta name='viewport' content='width=device-width, initial-scale=1' />"..
+"        <link rel='icon' href='data:image/x-icon;,' type='image/x-icon' />".. 
+"    </head>"..
+"    <body>"..
+"        <h1>Hello world!</h1>"..
+"        <p>node.heap() == "..node.heap().."</p>"..
+"        <p>node.chipid() == "..node.chipid().."</p>"..
+"        <ul>"..
+"            <li><a href='http://www.lua.org/manual/5.3/'>Lua Manual</a></li>"..
+"            <li><a href='http://www.nodemcu.com/docs/index/'>NodeMCU Docs</a></li>"..
+"        </ul>"..
+"    </body>"..
+"</html>\n"..
+"")
+    
+    client:close()
+    collectgarbage()
+        
+  end)
+end)
