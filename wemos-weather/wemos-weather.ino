ed96e89574d4cc3fd9f85dc6bdd7991e6c25ab9d
Travis Hardiman <travis.hardiman@gmail.com>
2017-11-30 00:43:53 -0500

    starting weather...

--- wemos-weather/wemos-weather.ino
+++ wemos-weather/wemos-weather.ino
@@ -3,6 +3,7 @@
 #include <ESP8266WiFi.h>
 // DNSServer - Version: Latest
 #include <DNSServer.h>
+#include <ESP8266HTTPClient.h>
 // ESP8266WebServer - Version: Latest
 #include <ESP8266WebServer.h>
 // WiFiManager - Version: Latest
@@ -22,6 +23,7 @@
 
 #define CONFIG  "/config.json"
 
+#define DEGREES_F  "°F"
 
 // wunderground constants * params
 #define WU_BASE_API "http://api.wunderground.com/api/"
@@ -43,25 +45,34 @@ char wu_bestfct[2] = "1";
 // using ZIP code, but increase size if other query is needed
 char wu_query[6] = "00000";
 
+String wu_url = ""; 
 
-// WeMos
-#define OLED_RESET 0  // GPIO0
+float wu_current_temp = 0.0;
+String wu_hi_temp = "";
+String wu_lo_temp = "";
+
+
+// WeMos OLED
 // SSD1306_SWITCHCAPVCC 0x2
 // SSD1306_I2C_ADDRESS:
 // 011110+SA0+RW - 0x3C or 0x3D
 // Address for 128x32 is 0x3C
 // Address for 128x64 is 0x3D (default) or 0x3C (if SA0 is grounded)
 #define OLED_ADDRESS 0x3C
+#define OLED_RESET 0  // GPIO0
 Adafruit_SSD1306 display(OLED_RESET);
 
+#define OLED_DELAY_MS 30000
+
+// WeMos SHT
 #define SHT3X_ADDRESS 0x45
 SHT3X sht30(SHT3X_ADDRESS);
 
 
 // HC-SR501 Motion Detector
-//TODO: test HC-SR501 pin
+//TODO: test HC-SR501 pin after shields mounted
 #define IR_PIN D1
-int ir_value;
+int ir_value = 0;
 
 
 void setup() {
@@ -97,17 +108,19 @@ void setup() {
         Serial.println("failed to load json config");
       }
       jsonFile.close();
+
+      wu_url = Wu_GetUrl();
     }
   }
 
   
-  // web server
+  // TODO: web server
   //wwwServer.reset(new ESP8266WebServer(WiFi.localIP(), 80));
   //wwwServer->on("/", Www_Root);
-  //wwwServer->on("/feed", Www_Feed);
   //wwwServer->onNotFound(Www_NotFound);
   //wwwServer->begin();
   //Serial.println("HTTP Server Started");
+
 
   // OLED setup
   display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDRESS);
@@ -119,11 +132,14 @@ void setup() {
 void loop() {
   // handle web requests
   //wwwServer->handleClient();
-  //TODO:
 
   ir_value = digitalRead(IR_PIN);
   Serial.println("IR value: ");
   Serial.println(ir_value);
+  if (ir_value == 1) {
+    // get temps from wu
+    // show temps
+  }
 
   delay(1000);
 }
@@ -138,18 +154,43 @@ File GetFile(String fileName) {
   return textFile;
 }
 
-String GetWuUrl() {
-  //return WU_BASE_API + wu_key + "/" +
-  //       WU_LANG + wu_lang + "/" +
-  //       WU_PWS + wu_pws + "/" +
-  //       WU_BESTFCT + wu_bestfct + "/" +
-  //       WU_FEATURES + "/" +
-  //       "q/" + wu_query +
-  //       "." + WU_FORMAT;
 
+// WU
+String Wu_GetUrl() {
   char * str;
   sprintf(str, "%a%b/%c%d/%e%f/%g%h/%i/q/%j.%k", WU_BASE_API, wu_key, WU_LANG, wu_lang, WU_PWS, wu_pws, WU_BESTFCT, wu_bestfct, WU_FEATURES, wu_query, WU_FORMAT);
   return str;
+}
+
+void Wu_Refresh() {
+  // GET wu_url
+  HTTPClient http;
+ 
+  http.begin(wu_url);
+  Serial.println(wu_url);
+  
+  int httpCode = http.GET();
+  Serial.println(httpCode);
+
+  if (httpCode > 0) {
+    int len = http.getSize();
+    // I bet I'm gonna have to stream that json...
+    DynamicJsonBuffer jsonBuffer;
+    //http.writeToStream(&Serial);
+    JsonObject& json = jsonBuffer.parseObject(http.getString());
+    // TODO: maybe use openweathermap instead?
+    if (json.success()) {
+      // WU data values to get:
+      // data.current_observation.temp_f (float)
+      // data.forecast.simpleforecast.forecastday[0].high.fahrenheit (string)
+      // data.forecast.simpleforecast.forecastday[0].low.fahrenheit (string)
+      wu_current_temp = json["data"]["current_observation"]["temp_f"];
+      wu_hi_temp = json["forecast"]["simpleforecast"]["forecastday"][0]["high"]["fahrenheit"].as<char*>();
+      wu_lo_temp = json["forecast"]["simpleforecast"]["forecastday"][0]["low"]["fahrenheit"].as<char*>();
+    }
+  }
+
+  http.end();
 }
 
 
@@ -157,15 +198,15 @@ String GetWuUrl() {
 void Oled_ShowTemps() {
   // Clear the buffer.
   display.clearDisplay();
-  display.setTextSize(1);
-  display.setCursor(0, 0);
-  display.setTextColor(WHITE);
 
   if (sht30.get() == 0) {
+    display.setCursor(0, 0);
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
     display.println("T: ");
     display.setTextSize(2);
     display.print(sht30.fTemp);
-    display.println("°F");
+    display.println(DEGREES_F);
 
     display.setTextSize(1);
     display.println("H: ");
@@ -173,19 +214,28 @@ void Oled_ShowTemps() {
     display.print(sht30.humidity);
     display.println("%");
   } else {
-    display.println("Error!");
+    display.println(":(");
   }
 
+  // show WU temps
+  display.setCursor(0, 0);
+  display.setTextColor(WHITE);
+  display.setTextSize(1);
+  display.print(wu_hi_temp);
+  display.println(DEGREES_F);
+  
+  display.setTextSize(2);
+  display.print(wu_current_temp);
+  display.println(DEGREES_F);
+
+  display.setTextSize(1);
+  display.print(wu_lo_temp);
+  display.println(DEGREES_F);
+
   display.display();
+
+  delay(OLED_DELAY_MS);
+  display.clearDisplay();
 }
 
-
-// WU data values to get:
-// data.current_observation.temp_f (float)
-// data.forecast.simpleforecast.forecastday[0].high.fahrenheit (string)
-// data.forecast.simpleforecast.forecastday[0].low.fahrenheit (string)
-
-//String GetTempFormat(String temp) {
-//	return temp + "°F";
-//}
 
