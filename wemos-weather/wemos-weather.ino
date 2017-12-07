e98f034fac5e90b507c34a00716565dae037e176
Travis Hardiman <travis.hardiman@gmail.com>
2017-12-06 22:22:31 -0500

    strings work, resume the rest of the coding...

--- wemos-weather/wemos-weather.ino
+++ wemos-weather/wemos-weather.ino
@@ -2,7 +2,7 @@
 // ESP8266WiFi - Version: Latest
 #include <ESP8266WiFi.h>
 // DNSServer - Version: Latest
-#include <DNSServer.h>
+//#include <DNSServer.h>
 #include <ESP8266HTTPClient.h>
 // ESP8266WebServer - Version: Latest
 #include <ESP8266WebServer.h>
@@ -25,6 +25,7 @@
 
 #define DEGREES_F  "°F"
 
+/*
 // wunderground constants * params
 #define WU_BASE_API "http://api.wunderground.com/api/"
 // Returns the API response in the specified language.
@@ -45,7 +46,31 @@ char wu_bestfct[2] = "1";
 // using ZIP code, but increase size if other query is needed
 char wu_query[6] = "00000";
 
-String wu_url = ""; 
+char * wu_url; 
+*/
+
+// try Strings
+// wunderground constants * params
+const String WU_BASE_API = "http://api.wunderground.com/api/";
+// Returns the API response in the specified language.
+const String WU_LANG = "lang:";
+// Use personal weather stations for conditions.
+const String WU_PWS = "pws:";
+// Use Weather Underground Best Forecast for forecast.
+const String WU_BESTFCT = "bestfct:";
+// features & format of this code
+const String WU_FEATURES = "conditions/forecast";
+const String WU_FORMAT = "json";
+
+// wunderground config params will be overwritten from CONFIG json file above
+String wu_key = "1234567890abcdef";
+String wu_lang = "EN";
+String wu_pws = "1";
+String wu_bestfct = "1";
+// using ZIP code, but increase size if other query is needed
+String wu_query = "00000";
+
+String wu_url;
 
 float wu_current_temp = 0.0;
 String wu_hi_temp = "";
@@ -61,17 +86,19 @@ String wu_lo_temp = "";
 #define OLED_ADDRESS 0x3C
 #define OLED_RESET 0  // GPIO0
 Adafruit_SSD1306 display(OLED_RESET);
-
+//const int MIDDLE_X = display.getLCDWidth() / 2;
+int MIDDLE_X = 0;
 #define OLED_DELAY_MS 30000
 
 // WeMos SHT
+// 0x44 when bridged or 0x45 unbridged
 #define SHT3X_ADDRESS 0x45
 SHT3X sht30(SHT3X_ADDRESS);
 
 
 // HC-SR501 Motion Detector
 //TODO: test HC-SR501 pin after shields mounted
-#define IR_PIN D1
+#define IR_PIN D3
 int ir_value = 0;
 
 
@@ -84,7 +111,6 @@ void setup() {
   Serial.println("Wifi connected ok. :)");
   Serial.println(WiFi.localIP());
 
-
   // init values from file system.
   if (SPIFFS.begin()) {
     Serial.println("mounted file system");
@@ -92,26 +118,46 @@ void setup() {
     // parse json config file
     File jsonFile = GetFile(CONFIG);
     if (jsonFile) {
+      Serial.println("opening json config");
       size_t size = jsonFile.size();
+      Serial.println("create buffer");
       // Allocate a buffer to store contents of the file.
       std::unique_ptr<char[]> jsonBuf(new char[size]);
+      Serial.println("read bytes");
       jsonFile.readBytes(jsonBuf.get(), size);
+      Serial.println("create DynamicJsonBuffer");
       DynamicJsonBuffer jsonBuffer;
+      Serial.println("jsonBuffer parseObject");
       JsonObject& json = jsonBuffer.parseObject(jsonBuf.get());
       if (json.success()) {
+        Serial.println("succeeded, copying strings");
+        /*
         strcpy(wu_key, json["wu_key"]);
         strcpy(wu_lang, json["wu_lang"]);
         strcpy(wu_pws, json["wu_pws"]);
         strcpy(wu_bestfct, json["wu_bestfct"]);
         strcpy(wu_query, json["wu_query"]);
+        */
+        wu_key = json["wu_key"].as<String>();
+        wu_lang = json["wu_lang"].as<String>();
+        wu_pws = json["wu_pws"].as<String>();
+        wu_bestfct = json["wu_bestfct"].as<String>();
+        wu_query = json["wu_query"].as<String>();
       } else {
         Serial.println("failed to load json config");
+        delay(10000);
       }
+      Serial.println("closing json config");
       jsonFile.close();
 
-      wu_url = Wu_GetUrl();
+      Serial.println("getting url");
+      delay(5000);
+      //wu_url = Wu_GetUrl();
+      //sprintf(wu_url, WU_BASE_API + "%s/%s%s/%s%s/%s%s/%s/q/%s.%s", wu_key.c_str(), WU_LANG.c_str(), wu_lang.c_str(), WU_PWS.c_str(), wu_pws.c_str(), WU_BESTFCT.c_str(), wu_bestfct.c_str(), WU_FEATURES.c_str(), wu_query.c_str(), WU_FORMAT.c_str());
+      wu_url = WU_BASE_API + wu_key + "/" + WU_LANG + wu_lang + "/" + WU_PWS + wu_pws + "/" + WU_BESTFCT + wu_bestfct + "/" + WU_FEATURES + "/q/" + wu_query + "." + WU_FORMAT;
     }
   }
+  Serial.println(wu_url);
 
   
   // TODO: web server
@@ -123,9 +169,12 @@ void setup() {
 
 
   // OLED setup
-  display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDRESS);
+  Serial.println("init OLED display");
+  //display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDRESS);
+  //MIDDLE_X = display.width() / 2;
 
   // IR sensor setup
+  Serial.println("init IR sensor");
   pinMode(IR_PIN, INPUT);
 }
 
@@ -138,6 +187,7 @@ void loop() {
   Serial.println(ir_value);
   if (ir_value == 1) {
     // get temps from wu
+    Wu_Refresh();
     // show temps
   }
 
@@ -156,11 +206,13 @@ File GetFile(String fileName) {
 
 
 // WU
-String Wu_GetUrl() {
+/*
+char * Wu_GetUrl() {
   char * str;
   sprintf(str, "%a%b/%c%d/%e%f/%g%h/%i/q/%j.%k", WU_BASE_API, wu_key, WU_LANG, wu_lang, WU_PWS, wu_pws, WU_BESTFCT, wu_bestfct, WU_FEATURES, wu_query, WU_FORMAT);
   return str;
 }
+*/
 
 void Wu_Refresh() {
   // GET wu_url
@@ -191,6 +243,13 @@ void Wu_Refresh() {
   }
 
   http.end();
+
+  Serial.print("Current: ");
+  Serial.println(wu_current_temp);
+  Serial.print("High: ");
+  Serial.println(wu_hi_temp);
+  Serial.print("Low: ");
+  Serial.println(wu_lo_temp);
 }
 
 
@@ -218,7 +277,8 @@ void Oled_ShowTemps() {
   }
 
   // show WU temps
-  display.setCursor(0, 0);
+  //TODO: test starting halfway: 
+  display.setCursor(MIDDLE_X, 0);
   display.setTextColor(WHITE);
   display.setTextSize(1);
   display.print(wu_hi_temp);
